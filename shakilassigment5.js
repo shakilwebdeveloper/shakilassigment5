8785cb092da13927b6f46ed1e78e0b65e90fad3f
shakilwebdeveloper <shakilfullstackwebdeveloper@gmail.com>
2025-08-30 23:59:48 +0600

    add now all file

---
+++ shakilassigment5.js
@@ -0,0 +1,9 @@
+const firstbtn = document.getElementById("btn1")
+console.log(firstbtn)
+
+const allbtn = document.getElementsByClassName("btn")
+console.log(allbtn)
+
+
+const allimage = document.getElementsByTagName("img")
+console.log(allimage)
